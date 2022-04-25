5857ff6fabde4668d2404b3ac160068e2213b9d5
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-04-25 19:10:54 +0530

    add file

--- Assignments/Arrays/repeated-twice.sh
+++ Assignments/Arrays/repeated-twice.sh
@@ -1,10 +1,13 @@
+#!/bin/bash -x
+
+
 read -p "enter the lower limit" lower
 read -p "enter the upper limit"upper
 
 index=0
 for((i=lower;i<=upper;))
 do
-if((i%11=0))
+if((i%11==0))
 then
 array[index++]=$i
 ((i+=11))
